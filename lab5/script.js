1bebec5b8cbf10ac200512c41f8c6b0e07f586e6
lomoriea <alylomo@gmail.com>
2026-09-24 13:38:27 -0400

    fixed the withdrawl money button

--- lab5/script.js
+++ lab5/script.js
@@ -1,29 +1,33 @@
-let playerHealth = 100;
-const damageAmount = 30;
+let accountMoney = 1000;
+const withdrawlAmount = 20;
 
 function takeMoney() {
-    playerHealth = playerHealth - damageAmount;
+    accountMoney = accountMoney - withdrawlAmount;
 
     const moneyText = document.getElementById("money-display");
     const statusText = document.getElementById("status-message");
 
-    healthText.innerText = playerHealth;
+    moneyText.innerText = accountMoney;
 
-    if(playerHealth > 0)
+    if(accountMoney > 0)
     {
-        moneyText.innerText = playerHealth;
-        statusText.innerText = "You've been hit!";
+        moneyText.innerText = accountMoney;
+        statusText.innerText = "$20 Withdrawn!";
     }
     else
     {
         moneyText.innerText = 0;
-        statusText.innerText = "Game Over";
+        statusText.innerText = "You're out of money!";
         statusText.style.color = "#f83722";
         statusText.style.fontweight = "bold";
         
         document.body.style.backgroundColor = "#5a1a1a";
         
         document.querySelector("button").disabled = true;
-        document.querySelector("button").innerText = "Dead";
+        document.querySelector("button").innerText = "No more money to withdrawl";
     }
+}
+
+function insertMoney() {
+
 }
